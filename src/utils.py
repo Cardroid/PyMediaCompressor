91a45755709763f32b6906e013e9849ada29ff87
Cardroid <number1dh@gmail.com>
2022-07-09 17:10:38 +0900

    Feat: 보조 함수 추가 (문자열, 파일 처리)

--- src/utils.py
+++ src/utils.py
@@ -45,3 +45,45 @@ def get_MD5_hash(filepath: str) -> str:
         for byte_block in iter(lambda: f.read(65536), b""):
             hasher.update(byte_block)
         return hasher.hexdigest()
+
+
+def overwrite_small_file(originFilepath: str, destinationFilepath: str, orginFileRemove=True) -> bool:
+    """원본 위치의 파일이 목적 위치의 파일 보다 작을 경우 덮어씁니다.
+
+    Args:
+        originFilepath (str): 원본 파일 경로
+        destFilepath (str): 목적 위치의 파일 경로
+        orginFileRemove (bool, optional): 원본 파일을 제거합니다. Defaults to True.
+
+    Returns:
+        bool: 목적 위치의 파일이 덮어써진 경우 True, 아닐 경우 False를 반환합니다.
+    """
+
+    assert os.path.isfile(originFilepath) and os.path.isfile(destinationFilepath), "원본 또는 목적 파일이 존재하지 않습니다."
+
+    orig_file_size = os.path.getsize(originFilepath)
+    dest_file_size = os.path.getsize(destinationFilepath)
+
+    is_need_remove = orig_file_size < dest_file_size
+    is_remove_success = False
+
+    if is_need_remove:
+        try:
+            os.replace(originFilepath, destinationFilepath)
+            is_remove_success = True
+        except:
+            is_remove_success = False
+    elif orginFileRemove:
+        try:
+            os.remove(originFilepath)
+        except:
+            pass
+
+    return is_remove_success
+
+
+def is_str_empty_or_space(string: str) -> bool:
+    if string == None or string == "" or string.isspace():
+        return True
+    else:
+        return False
